3eff7d050667c311c7fae93b1223384bb8d03c8a
WillardBowles <relic.dex@gmail.com>
2023-02-10 23:14:48 -0800

    Spring2023 Willard Bowles Lab4

--- src/Radians.java
+++ src/Radians.java
@@ -1,13 +1,25 @@
+/**
+ *
+ * @author Trevor Hartman
+ * @author Willard Bowles
+ *
+ * @since Version 1.0
+ *
+ */
+
+
 import java.util.Scanner;
 
 public class Radians {
 
-    public static <ADD TYPE> toRadians(<ADD TYPE> degrees) {
-        // Pur your code here
+    public static double toRadians(double degrees) {
+        // Calculate the conversion from degrees to radians
+        return degrees * Math.PI / 180.0;
     }
 
-    public static <ADD TYPE> toDegrees(<ADD TYPE> radians) {
-        // Put your code here
+    public static double toDegrees(double radians) {
+        // Calculate the conversion from radians to degrees
+        return radians * 180.0 / Math.PI;
     }
 
     public static void main(String[] args) {
@@ -16,11 +28,17 @@ public class Radians {
         System.out.print("Type degrees you'd like converted to radians: ");
         double degrees = Double.valueOf(scanner.nextLine());
 
-        // Add System.out, your method call, and Math.toRadians call here
+        double radians = toRadians(degrees);
+        double mathRadians = Math.toRadians(degrees);
+        System.out.printf("Your conversion to radians: %f\n", radians);
+        System.out.printf("Math.toRadians conversion: %f\n", mathRadians);
 
         System.out.print("Type radians you'd like converted to degrees: ");
-        double radians = Double.valueOf(scanner.nextLine());
+        radians = Double.valueOf(scanner.nextLine());
 
-        // Add System.out, your method call, and Math.toDegrees call here
+        degrees = toDegrees(radians);
+        double mathDegrees = Math.toDegrees(radians);
+        System.out.printf("Your conversion to degrees: %f\n", degrees);
+        System.out.printf("Math.toDegrees conversion: %f\n", mathDegrees);
     }
 }
--- src/SimpleMethod.java
+++ src/SimpleMethod.java
@@ -1,8 +1,10 @@
 public class SimpleMethod {
-    // Put your method code here
+    public static void square(int num) {
+        int result = num * num;
+        System.out.println("The result of squaring " + num + " is: " + result);
+    }
 
     public static void main(String[] args) {
-        // This is the method call, create the square method.
         square(25);
     }
 }
